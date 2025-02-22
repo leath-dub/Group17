ac8bf9919c6d293764c5e9916cee7b10f27355ad
leath-dub <fierceinbattle@gmail.com>
2025-02-22 14:18:56 +0000

    Stub openai integration with function calling and system message

--- agent/__main__.py
+++ agent/__main__.py
@@ -1,5 +1,67 @@
+import vars
+
+from openai import OpenAI
+
 def main() -> None:
-    print("Hello, world!")
+    client = OpenAI()
+    tools = [{
+        "type": "function",
+        "function": {
+            "name": "dupe_pod",
+            "description": "Create a new instance of pod given the pod id string",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                  "id": {
+                      "type": "string",
+                      "description": "The human readable id for the pod"
+                  }
+                },
+                "required": ["id"],
+                "additionalProperties": False
+            },
+            "strict": True
+        }
+    }]
+
+    example_json = """
+    {
+      "trigger": {
+        "pod": "water-duck",
+        "cpu": {
+          "op": ">",
+          "value": "80%",
+        }
+      },
+      "pods": [
+        {
+            "id": "water-duck",
+            "metrics": {
+              "cpu": "80%",
+              "memory": "50%",
+            }
+        },
+        {
+            "id": "grand-major",
+            "metrics": {
+              "cpu": "10%",
+              "memory": "5%",
+            }
+        }
+      ],
+    }
+    """
+
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[{"role": "system", "content": vars.SYSTEM_MESSAGE}, {"role": "user", "content": example_json}],
+        tools=tools
+    )
+
+    for call in completion.choices[0].message.tool_calls:
+        assert(call.type == "function")
+        print(call)
 
 if __name__ == "__main__":
+
     main()
