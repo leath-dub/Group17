bbff3883d2d52c7ebb9456787b12e20920a83973
Niall Ryan <niallcryan@live.ie>
2025-02-23 03:23:59 +0000

    fixed massive blocker

--- agent/app.py
+++ agent/app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 @app.route("/alert", methods=["POST"])
 def alert():
-    print("AHHHH PROD IS DOWN :(")
+    print("AHHHH PROD IS DOWN :(", flush=True)
     if request.content_type != "application/json":
         return "Content-Type must be application/json", 400
 
     data = request.get_json()
     agent.forward_alert(data)
 
-    return "", 200
+    return "TEST", 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080, debug=True)
